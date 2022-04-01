606951be22b67859ec6e18af21e52daccaf6278f
fawzy ahmed <fawzy.fawzy46@gmail.com>
2022-04-01 13:58:47 +0200

    updates

--- user_auth.js
+++ user_auth.js
@@ -2,11 +2,16 @@
  {
 	const username = document.getElementById("user");
 	const password = document.getElementById("Password");
+	const err = document.getElementById("error-box");
 	if (username.value === "fawzy" && password.value === "fawzy") {
+		if(err.style.display === "flex")
+		{
+			err.style.display = "none";
+		}
         alert("You have successfully logged in.");
-        window.location.replace("movies.html");
+        window.location.href = "movies.html";
     } 
 	else {
-        alert("user and/or Password are not correct");
+		err.style.display = "flex";
     } 
  }
